1b522bc68f6d0a040d98f6678fe17b78e7cdfe9e
Juris-Berkulis <krztfjrbhm1@jmail.com>
2022-05-04 00:52:31 +0300

    1. Remove commented code; 2. Change automatically one file for "firebase" hosting.

--- src/route_components/TasksForToday/TasksForToday.js
+++ src/route_components/TasksForToday/TasksForToday.js
@@ -22,80 +22,6 @@ export const TasksForToday = () => {
     const tasksSignForTasksSortingSel = useSelector(getTasksListTasksSignForTasksSortingSelector);
     const reverseDirectionForTasksSortinBySignSel = useSelector(getTasksListReverseDirectionForTasksSortinBySignSelector);
 
-    // const changeTask = (taskObject) => {
-    //     dispatch({
-    //         type: editableTaskObjectAction.type,
-    //         payload: taskObject,
-    //     });
-
-    //     history(allAppComponentsWithPageTitle.edittask.path);
-    // };
-
-    // const deleteTask = (taskID) => {
-    //     const thisTaskWillBeDeleted = tasksKindOfDictByUserUIDSel[taskID];
-
-    //     for (let deleteTaskSign in thisTaskWillBeDeleted) {
-    //         let deleteTaskSignIsFind = false;
-    //         for (let specificTaskId in tasksKindOfDictByUserUIDSel) {
-    //             if (tasksKindOfDictByUserUIDSel[specificTaskId][deleteTaskSign]) {
-    //                 if (+specificTaskId === taskID) {
-    //                     continue;
-    //                 } else if (+specificTaskId !== taskID) {
-    //                     if (tasksKindOfDictByUserUIDSel[specificTaskId][deleteTaskSign] === thisTaskWillBeDeleted[deleteTaskSign]) {
-    //                         deleteTaskSignIsFind = true;
-    //                         break;
-    //                     } else if (tasksKindOfDictByUserUIDSel[specificTaskId][deleteTaskSign] !== thisTaskWillBeDeleted[deleteTaskSign]) {
-    //                         continue;
-    //                     }
-    //                 }
-    //             }
-    //         }
-
-    //         if (!deleteTaskSignIsFind) {
-    //             dispatch(deleteExtraSignOfTaskFilteringWithThunkAction(deleteTaskSign, thisTaskWillBeDeleted[deleteTaskSign]));
-    //         }
-    //     }
-
-    //     deleteTaskWithThunkAction(taskID);
-    // };
-
-    // const sortTasksBySign = (itemA, itemB) => {
-    //     let propertyA;
-    //     let propertyB;
-
-    //     if (tasksSignForTasksSortingSel === allSignsForTasksFilter.taskCreateAt.variable || !tasksSignForTasksSortingSel) {
-    //         propertyA = +itemA.taskID;
-    //         propertyB = +itemB.taskID;
-    //     } else {
-    //         propertyA = itemA[tasksSignForTasksSortingSel];
-    //         propertyB = itemB[tasksSignForTasksSortingSel];
-    //     }
-
-    //     let tasksSortingDirectionSwitch = 1;
-    //     if (reverseDirectionForTasksSortinBySignSel) {
-    //         tasksSortingDirectionSwitch = -1;
-    //     }
-
-    //     if (propertyA > propertyB) {
-    //         return 1 * tasksSortingDirectionSwitch
-    //     } else if (propertyA < propertyB) {
-    //         return -1 * tasksSortingDirectionSwitch
-    //     } else {
-    //         return 0
-    //     }
-    // };
-
-    // const openTheTask = (item) => {
-    //     dispatch({
-    //         type: openTaskAction.type,
-    //         payload: item,
-    //     });
-    // };
-
-    // const deleteTheTaskFromListWithTasksForToday = (taskID) => {
-    //     dispatch(deleteTheTaskFromListWithTasksForTodayWithThunkAction(taskID));
-    // };
-
     const tasksListTasksKindOfListByIdSelForProps = tasksKindOfListByUserUIDSel
     .filter((item) => {
         return (
@@ -105,33 +31,6 @@ export const TasksForToday = () => {
     .filter((item) => {
         return (
             tasksFiltering(item, dictWithListsForTasksFilterSel, allSignsForTasksFilter)
-            // dictWithListsForTasksFilterSel
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskID.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskID.variable][item.taskID]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskCreateAt.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskCreateAt.variable][item.taskCreateAt]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskCategory.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskCategory.variable][item.taskCategory]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskName.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskName.variable][item.taskName]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.subtaskName.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.subtaskName.variable][item.subtaskName]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskControl.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskControl.variable][item.taskControl]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskDeadline.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskDeadline.variable][item.taskDeadline]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskDuration.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskDuration.variable][item.taskDuration]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskImportance.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskImportance.variable][item.taskImportance]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskPriority.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskPriority.variable][item.taskPriority]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskStatus.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskStatus.variable][item.taskStatus]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskUrgency.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskUrgency.variable][item.taskUrgency]
-            // && 
-            // dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskComment.variable] && dictWithListsForTasksFilterSel[allSignsForTasksFilter.taskComment.variable][item.taskComment]
         )
     })
     .sort((itemA, itemB) => sortTasksBySign(itemA, itemB, tasksSignForTasksSortingSel, reverseDirectionForTasksSortinBySignSel))
--- src/ui_components/AllTasksUI.jsx
+++ src/ui_components/AllTasksUI.jsx
@@ -4,31 +4,5 @@ import { TasksPage } from '../widget_components/TasksPage/TasksPage';
 export const AllTasksUI = (props) => {
     return (
         <TasksPage allAppComponentsWithPageTitle={props.allAppComponentsWithPageTitle} tasksListTasksKindOfListByIdSelForProps={props.tasksListTasksKindOfListByIdSelForProps} dictWithListsForTasksFilterSel={props.dictWithListsForTasksFilterSel} changeTask={props.changeTask} deleteTask={props.deleteTask} dispatch={props.dispatch} tasksKindOfDictByUserUIDSel={props.tasksKindOfDictByUserUIDSel} history={props.history}></TasksPage>
-        // <div className={props.classes.allTasks}>
-        //     <OpenTask changeTask={props.changeTask} deleteTask={props.deleteTask} dispatch={props.dispatch} tasksKindOfDictByUserUIDSel={props.tasksKindOfDictByUserUIDSel} history={props.history}></OpenTask>
-        //     <div className={props.classes.allTasks__filterWrapper}>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskCategory || {}} signForTasksFilter={allSignsForTasksFilter.taskCategory.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskName || {}} signForTasksFilter={allSignsForTasksFilter.taskName.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.subtaskName || {}} signForTasksFilter={allSignsForTasksFilter.subtaskName.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskDeadline || {}} signForTasksFilter={allSignsForTasksFilter.taskDeadline.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskDuration || {}} signForTasksFilter={allSignsForTasksFilter.taskDuration.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskImportance || {}} signForTasksFilter={allSignsForTasksFilter.taskImportance.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskPriority || {}} signForTasksFilter={allSignsForTasksFilter.taskPriority.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskUrgency || {}} signForTasksFilter={allSignsForTasksFilter.taskUrgency.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskControl || {}} signForTasksFilter={allSignsForTasksFilter.taskControl.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskStatus || {}} signForTasksFilter={allSignsForTasksFilter.taskStatus.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskCreateAt || {}} signForTasksFilter={allSignsForTasksFilter.taskCreateAt.variable}></TasksFilter>
-        //         <TasksFilter propertiesForTasksFilter={props.dictWithListsForTasksFilterSel.taskComment || {}} signForTasksFilter={allSignsForTasksFilter.taskComment.variable}></TasksFilter>
-        //     </div>
-        //     <ul className={props.classes.allTasks__tasksList}>
-        //         {
-        //             props.tasksListTasksKindOfListByIdSelForProps.length
-        //             ? 
-        //             props.tasksListTasksKindOfListByIdSelForProps
-        //             : 
-        //             <p className={props.classes.allTasks__tasksEmptyListText}>Список задач пуст</p>
-        //         }
-        //     </ul>
-        // </div>
     )
 };
